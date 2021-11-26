f0636c2fab38193c536e64f6223ef9c590766fac
Anjali-abc <90022797+Anjali-abc@users.noreply.github.com>
2021-11-26 14:21:18 +0530

    Add files via upload

---
+++ objectcreation.js
@@ -0,0 +1,27 @@
+var person={
+
+ p1:{
+     FirstName:"Ram",
+     LastName:"sharma",
+     OfficeAddress:"Boring road,Patna",
+     },
+  p2:{
+     FirstName:"Shyam",
+     LastName:"Verma",
+     OfficeAddress:"Kankarbag,Patna",
+     },
+   p3:{
+       FirstName:"Mohan",
+       LastName:"Kumar",
+       OfficeAddress:"Rajindranagar,Patna",
+       },
+      };
+console.log(person.p1.FirstName);
+console.log(person.p1.LastName);
+console.log(person.p1.OfficeAddress);
+console.log(person.p2.FirstName);
+console.log(person.p2.LastName);
+console.log(person.p2.OfficeAddress);
+console.log(person.p3.FirstName);
+console.log(person.p3.LastName);
+console.log(person.p3.OfficeAddress);
